89360baeec7254ed2a3beb1c8503d6b52639ef88
morozvitali <morozvitali99@gmail.com>
2022-10-30 03:00:56 +0300

    bugfix 'Start All Systems'

--- Car.java
+++ Car.java
@@ -1,2 +1,23 @@
 public class Car {
+
+    public void start() {
+        System.out.println("Start Systems: ");
+        startElectricity();
+        startCommand();
+        startFuelSystem();
+    }
+
+    private void startElectricity() {
+        System.out.println("Start Elictricity Systems...");
+    }
+
+    private void startCommand() {
+        System.out.println("Start Command Systems...");
+    }
+
+    private void startFuelSystem() {
+        System.out.println("Start Fuel Systems...");
+    }
+
+
 }
